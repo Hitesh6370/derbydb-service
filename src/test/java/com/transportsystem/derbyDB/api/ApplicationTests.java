437167b142215fc8a9d84f0899ebddb1dea6a931
Vickky Singh <hitesh6370@gmail.com>
2020-04-14 16:25:52 +0200

    update yml and test run fix

--- src/test/java/com/transportsystem/derbyDB/api/ApplicationTests.java
+++ src/test/java/com/transportsystem/derbyDB/api/ApplicationTests.java
@@ -1,11 +1,21 @@
 package com.transportsystem.derbyDB.api;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
 
 }
